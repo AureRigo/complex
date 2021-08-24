b899e720d8fcc839497764c2e39b2bfc5fd36c31
AureRigo <aurelienrigolet3@gmail.com>
2021-08-24 16:57:27 +0200

    I'm working on my device component and I stop my course at the Property binding

---
+++ src/app/device/device.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device',
+  templateUrl: './device.component.html',
+  styleUrls: ['./device.component.scss']
+})
+export class DeviceComponent implements OnInit {
+
+  deviceName: string = 'Machine à laver';
+  deviceStatus: string = 'éteint';
+  
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  getStatus() {
+    return this.deviceStatus;
+  }
+
+}
